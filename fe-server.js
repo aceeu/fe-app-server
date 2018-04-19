fea3812d7366d57b9ba6b5e8e8850391bda5c61e
aceeu <ace.evg@gmail.com>
2018-04-19 22:25:57 +0300

    update user login, became a bit more secured

--- fe-server.js
+++ fe-server.js
@@ -2,12 +2,23 @@ let express = require('express');
 let session = require('express-session');
 let MongoClient = require('mongodb').MongoClient;
 let config = require('./config');
+let jsSHA = require('jssha');
 const bodyParser = require('body-parser');
 const addData = require('./add-data');
 const detectValidUser = require('./helpers').detectValidUser;
 
 const dbName = config.db_name;
 let app = express();
+let tokenGenerator = require( 'token-generator' )({
+        salt: ';lasdkfj alskdf alskd flk;lksdfalsdk d',
+        timestampMap: 'abcdefghij', // 10 chars array for obfuscation proposes
+    });
+
+function jssha(text) {
+  const shaObj = new jsSHA("SHA-256", "TEXT");
+  shaObj.update(text);
+  return shaObj.getHash("HEX");
+}
 
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 * 60 }}));
 
@@ -29,9 +40,29 @@ app.use((req, res, next) => {
     next();
 })
 
+app.get('/authtoken', (req, res, next) => {
+  try {
+    if (req.session) {
+      req.session.tokenGenerator = tokenGenerator.generate();
+      res.json({res: true, token: req.session.tokenGenerator});
+    } else {
+      res.json({res: false});
+    }
+  } finally {
+      res.end();
+  }
+});
 
 app.post('/auth', async function(req, res, next) {
     try {
+        const clientHash = req.body.hash;
+        console.log(req.body.password + '-' + req.session.tokenGenerator);
+        const serverHash = jssha(req.body.password + req.session.tokenGenerator);
+        if ( serverHash !== clientHash) {
+            console.log('client hash:' + clientHash);
+            console.log ('server hash:' + serverHash);
+            throw 'hash';
+          }
         const client = await MongoClient.connect(config.database_url);
         const users = client.db(dbName).collection('users');
         const fres = await users.find({user: {$eq: req.body.user}, password: {$eq: req.body.password}});
@@ -41,7 +72,7 @@ app.post('/auth', async function(req, res, next) {
                 req.session.name = items[0].user;
                 res.json({res: true, name: req.session.name});
             }
-            else 
+            else
                 res.json({res: false, text: 'no valid user'});
         }
         else {
@@ -53,15 +84,15 @@ app.post('/auth', async function(req, res, next) {
     } finally {
         res.end();
     }
-    
-});  
+
+});
 
 app.get('/logout', function(req, res, next) {
     if (req.session.name) {
         delete req.session.name;
         res.json({res: true});
     } else {
-        res.json({res: false, text: 'no user logged before'})
+        res.json({res: true, text: 'no user logged before'})
     }
     res.end();
 });
@@ -69,7 +100,7 @@ app.get('/logout', function(req, res, next) {
 app.get('/user', function(req, res, next){
     if (req.session.name)
         res.json({res: true, name: req.session.name});
-    else   
+    else
         res.json({res: false, text: 'no session name'});
     res.end();
 });
