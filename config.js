1e6868327af2f4a4b503c478d790d94f6d14db4c
aceeu <ace.evg@gmail.com>
2018-09-19 21:16:24 +0300

    update import-csv

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.database_url = 'mongodb://localhost:27017';
+exports.database_url = 'mongodb+srv://aceeu:1qa2ws3ed@cluster0-fg6zf.mongodb.net/test?retryWrites=true';
 exports.db_name = 'fe';
 exports.public_folder = '../fe-app/build';
--- import-csv.js
+++ import-csv.js
@@ -1,11 +1,12 @@
 let fs = require('fs');
 let parse = require('csv-parse');
 let asyncc = require('async');
-var inputFile='./rashod.rashod.7-21-18.dump.csv';
+var inputFile='./rashod.rashod.9-19-18.dump.csv';
 let config = require('./config');
 let MongoClient = require('mongodb').MongoClient;
 
 // импорт csv файла экспортированный из http://192.168.1.34/phpliteadmin.php?table=rashod&action=table_export
+//----- удаляет предыдущие данные
 
 function make(line) {
   const date = new Date(line[2]);
@@ -15,16 +16,19 @@ function make(line) {
 
 
 (async function() {
-  const client = await MongoClient.connect(config.database_url);
-  const collection = client.db(config.db_name).collection('data');
-  var parser = parse({delimiter: ';'}, function (err, data) {
-    asyncc.eachSeries(data, function (line, callback) {
-      collection.insertOne(make(line)).then(() => {
-        callback();
+  try{
+    const client = await MongoClient.connect(config.database_url);
+    const collection = client.db(config.db_name).collection('data');
+    var parser = parse({delimiter: ';'}, function (err, data) {
+      asyncc.eachSeries(data, function (line, callback) {
+        collection.insertOne(make(line)).then(() => {
+          callback();
+        });
       });
+      console.log('end');
     });
-    console.log('end');
-  });
-
-  fs.createReadStream(inputFile).pipe(parser);
+    fs.createReadStream(inputFile).pipe(parser);
+  } catch(e) {
+    console.log('catch:' + e.toString());
+  }
 })();
