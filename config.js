a2a1a9b567050df5aba39ca6a4900e03ffcff9ea
aceeu <ace.evg@gmail.com>
2018-09-21 09:32:19 +0300

    update config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.database_url = 'mongodb+srv://aceeu:1qa2ws3ed@cluster0-fg6zf.mongodb.net/test?retryWrites=true';
+let dbAuth = require('./dbpasswd');
+exports.database_url = `mongodb+srv://${dbAuth.user}:${dbAuth.pwd}@cluster0-fg6zf.mongodb.net/test?retryWrites=true`;
 exports.db_name = 'fe';
 exports.public_folder = '../fe-app/build';
